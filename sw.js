66174b31ac491b173213ab606ed2e38c2d380072
Khatrao84 <133596110+Khatrao84@users.noreply.github.com>
2026-01-10 20:54:29 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// UPDATED TO VERSION 1 (Forces update)
-const CACHE_NAME = 'gurbani-app-v1'; 
+// UPDATED TO VERSION 1.1 (Forces update)
+const CACHE_NAME = 'gurbani-app-v1.1'; 
 
 const ASSETS = [
   '/',
